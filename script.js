9e51fcfbd9b9ada848e2999652b165b5a9b119b9
Margaret Ralston <pandahart@gmail.com>
2020-12-10 14:44:29 -0800

    password generation works, the list of possible characters is based on the checked/unchecked options, and the length is correct. NO VALIDATION! The password may or may NOT contain at least 1 of each selected character type.

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var pwLength = document.querySelector("#pwLength");
+var optUC = document.querySelector("#uppercase");
+var optLC = document.querySelector("#lowercase");
+var optNM = document.querySelector("#numbers");
+var optSC = document.querySelector("#specialCharacters");
 
-function generatePassword() {
-	var options = {
-		length: 8,
-		include: {
-			letters: "abcdefghijklmnopqrstuvwxyz",
-			characters: "!@#$%^&*()-_=+/\\'\"[]{}|?.,;:"
+var options = {
+		length: pwLength.value,
+		criteria: {
+			lowercase: "abcdefghijklmnopqrstuvwxyz",
+			uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+			characters: "!@#$%^&*()-_=+/\\'\"[]{}|?.,;:`~<>",
+			numbers: "0123456789"
 		}
 	}
 
-	return "password";
+function generatePassword() {
+	// only update the length value if it's within range
+	if(pwLength.value > 7 && pwLength.value < 129) {
+		options.length = pwLength.value;
+	}
+
+	var criteria = "", password = "";
+
+	if(optLC.checked) {
+		criteria += options.criteria.lowercase;
+	}
+	if(optUC.checked) {
+		criteria += options.criteria.uppercase;
+	}
+	if(optNM.checked) {
+		criteria += options.criteria.numbers;
+	}
+	if(optSC.checked) {
+		criteria += options.criteria.characters;
+	}
+
+	// if everything was UNchecked,
+	// ensure that at least lowercase letters are included
+	if(criteria === "") {
+		criteria = options.criteria.lowercase;
+	}
+
+	for(var i = 0; i < options.length; i++) {
+		// get a random index for a single character in the criteria string
+		randomIndex = Math.floor(Math.random() * criteria.length);
+
+		// append the randomly selected character from the
+		// criteria string to build the password
+		password += criteria[randomIndex];
+	}
+
+	return password;
 }
 
 // Write password to the #password input
