b040f84ade395d11d390c7f7fe37bfb6f35974f8
Margaret Ralston <pandahart@gmail.com>
2020-12-10 16:06:10 -0800

    added regex checks and a while loop to verify the password contains at least one of each selected character type.

--- script.js
+++ script.js
@@ -23,6 +23,15 @@ function generatePassword() {
 	}
 
 	var criteria = "", password = "";
+	var randomCriteriaListIndex, randomCharIndex;
+	var hasLC = true, hasUC = true, hasNM = true, hasSC = true;
+	// regex checks: https://stackoverflow.com/a/61550284
+	var rxLC = /[a-z]/g;
+	var rxUC = /[A-Z]/g;
+	var rxNM = /[0-9]/g;
+	var rxSC = /[!@#$%^&*()-_=+/\\'\"\[\]{}|?\.,;:`~<>]/g;
+
+	var regex = null;
 
 	if(optLC.checked) {
 		criteria += options.criteria.lowercase;
@@ -43,13 +52,46 @@ function generatePassword() {
 		criteria = options.criteria.lowercase;
 	}
 
-	for(var i = 0; i < options.length; i++) {
-		// get a random index for a single character in the criteria string
-		randomIndex = Math.floor(Math.random() * criteria.length);
+	var completed = false;
+	while(!completed) {
+		password = "";
+		for(var i = 0; i < options.length; i++) {
 
-		// append the randomly selected character from the
-		// criteria string to build the password
-		password += criteria[randomIndex];
+			// get a random index for a single character in the criteria string
+			randomIndex = Math.floor(Math.random() * criteria.length);
+
+			// append the randomly selected character from the
+			// criteria string to build the password
+			password += criteria[randomIndex];
+		}
+
+		if( optLC.checked && rxLC.test(password) ) {
+			hasLC = true;
+		} else if(optLC.checked) {
+			hasLC = false;
+		}
+
+		if( optUC.checked && rxUC.test(password) ) {
+			hasUC = true;
+		} else if(optUC.checked) {
+			hasUC = false;
+		}
+
+		if( optNM.checked && rxNM.test(password) ) {
+			hasNM = true;
+		} else if(optNM.checked) {
+			hasNM = false;
+		}
+
+		if( optSC.checked && rxSC.test(password) ) {
+			hasSC = true;
+		} else if(optSC.checked) {
+			hasSC = false;
+		}
+
+		if(hasLC === true && hasUC === true && hasNM === true && hasSC === true) {
+			completed = true;
+		}
 	}
 
 	return password;
